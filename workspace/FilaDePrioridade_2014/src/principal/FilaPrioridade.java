47b4ff512e6b5c86b15b8c78855950de2ceede58
Rafael Tavares Andrade <rafaeltavaresandrade@gmail.com>
2015-06-08 16:36:19 -0300

    alterações projetos

---
+++ workspace/FilaDePrioridade_2014/src/principal/FilaPrioridade.java
@@ -0,0 +1,39 @@
+package principal;
+
+import br.com.arvoreBinariaDeBusca.ArvoreBinariaBuscaNova;
+
+
+public class FilaPrioridade<T extends Comparable<T>> {
+	
+	ArvoreBinariaBuscaNova<T> arvore;
+	
+	public FilaPrioridade(){
+		arvore = new ArvoreBinariaBuscaNova<T>();
+	}
+	
+	public void Inserir(T elemento){
+		arvore.inserir(elemento);
+	}
+	
+	public void Remover(){
+		arvore.RemoverMaior();
+	}
+	public void RetornarMaior(){
+		arvore.getMaior();
+	}
+	
+	public String toString2(){
+		return arvore.imprimir();
+	}
+	
+	public String toString(){
+		return arvore.toString2();
+	}
+	public int tamanho(){
+		return this.arvore.tamanho();
+	}
+	public boolean ehVazio(){
+		return this.arvore.ehVazio();
+	}
+
+}
